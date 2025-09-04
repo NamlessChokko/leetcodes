3b96adde8be97815e978df659b08d3707a2ae380
NamlessChokko <osmaoficial0@gmail.com>
2025-09-04 17:47:14 -0400

    refactor: 03

--- 03-longest-substring-without-repeating-characters.c
+++ 03-longest-substring-without-repeating-characters.c
@@ -1,28 +1,21 @@
 #include <stdbool.h>
-#include <stdlib.h>
-
-#define CHARS 256
 
 int lengthOfLongestSubstring(char *s) {
   int max_count = 0;
-  int i = 0;
   int j = 0;
 
-  while (s[i] != '\0') {
-    bool *table = (bool *)calloc(CHARS, sizeof(bool));
+  for (int i = 0; s[i]; i++){
+    bool table[128] = {0};
     char current = s[i];
-    int index_count = 0;
     j = i;
 
     while (s[j] != '\0' && !table[(int)current]) {
       table[(int)current] = true;
       current = s[++j];
-      index_count++;
     }
 
-    free(table);
-    max_count = (index_count > max_count) ? index_count : max_count;
-    i++;
+    int length = j - i;
+    max_count = (length > max_count) ? length : max_count;
   }
 
   return max_count;
